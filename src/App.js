cee569f857957d29324726b1e0172f93a74a59eb
Abu sufyan <meharabusufyan429@gmail.com>
2026-05-05 11:15:04 +0500

    create two files name LearnClearn LearnCode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import LearnAos from './Components/LearnAos';
+import LearnClear from './Components/LearnClear';
+import LearnCode from './Components/LearnCode';
 
 function App() {
   return (
     <div className="App">
    <LearnAos/>
+      <LearnClear/>
+      <LearnCode/>
     </div>
   );
 }
---
+++ src/Components/LearnClear.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LearnClear = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default LearnClear
